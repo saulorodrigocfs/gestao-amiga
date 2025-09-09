b95288db10b584f013281faf47ea4ab1c6f048f2
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-09-09 18:07:07 -0300

    Usuário, Clientes, Vendas, Produtos, estrutura básica

--- gestao/admin.py
+++ gestao/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Cliente #importa o modelo Cliente
+from .models import Cliente, PerfilUsuario, Produto, Venda #importa o modelo Cliente
 
-admin.site.register(Cliente) #Registra o modelo no admin
+#Registrar o modelo no admin
+admin.site.register(Cliente) 
+admin.site.register(PerfilUsuario)
+admin.site.register(Produto)
+admin.site.register(Venda)
--- gestao/models.py
+++ gestao/models.py
@@ -1,9 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+#Cliente que cada usuário vai cadastrar
 class Cliente(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     email = models.EmailField()
+    telefone = models.CharField(max_length=20, blank = True)
+    endereco = models.CharField(max_length=200, blank = True)
     data_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nome
+
+#Perfil estendido usuário
+class PerfilUsuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefone = models.CharField(max_length=20, blank = True)
+    endereco = models.CharField(max_length=200, blank = True)
+
+    def __str__(self):
+        return self.usuario.username
+    
+#Produto do usuário/loja
+class Produto(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    estoque = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nome
+    
+#Venda realizada
+class Venda(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.produto.nome} - {self.quantidade}"
+    
