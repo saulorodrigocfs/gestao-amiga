47a6c3150266d8298c815d59633147455f5db604
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-10-17 19:07:06 -0300

    Adição do .env e ocultação de email e senha

--- gestao_amiga/settings.py
+++ gestao_amiga/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +133,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'saulorodrigocfs@gmail.com'
-EMAIL_HOST_PASSWORD = 'Cefcef7*'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
