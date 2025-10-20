2e8644b2ecc359718ebaebca5e63f7aad31f4a3c
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-10-20 16:21:24 -0300

    Ajustes no .env 'config'

--- gestao_amiga/settings.py
+++ gestao_amiga/settings.py
@@ -137,6 +137,8 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'base'
 LOGOUT_REDIRECT_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
