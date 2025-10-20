553d41580349e4bf9b4ab96594aec37676a503ef
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-10-20 14:43:11 -0300

    ajuste no .env

--- gestao_amiga/settings.py
+++ gestao_amiga/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,9 +136,6 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'base'
 LOGOUT_REDIRECT_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp@gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
