8f40e2c00d9f6d142fdd4c9d2e426bf4b54f365e
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-10-20 14:36:45 -0300

    ajustes no settings.py para deploy teste

--- gestao_amiga/settings.py
+++ gestao_amiga/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'gestao_amiga.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'saulorodrigocfs.mysql.pythonanywhere-services.com',
+        'NAME': 'saulorodrigocfs$gestaoamiga',
+        'USER': 'saulorodrigocfs',
+        'PASSWORD': 'Cefcef7*',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
